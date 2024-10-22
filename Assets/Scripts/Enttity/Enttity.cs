73c42e5e2d19154174525895aec8af3b700fb291
Arong2013 <111150462+Arong2013@users.noreply.github.com>
2024-10-22 17:17:06 +0900

    수정

---
+++ Assets/Scripts/Enttity/Enttity.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//게임 내에서 물리적 영향을 가진 모든 오브젝트 
+public abstract class Enttity : MonoBehaviour
+{
+    Field parentField;
+}
